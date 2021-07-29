e4398df06c84a71caf22427fdf7d6e650c3b2757
JUNYOUNG <roywnsdud31@gmail.com>
2021-07-29 17:33:26 +0900

    workshop7 update

--- 02_Homeworkshop/python_07_workshop.py
+++ 02_Homeworkshop/python_07_workshop.py
@@ -1,52 +1,34 @@
-# 1. pip 
+# 1. pip
 
-# bash 에서 $ pip install faker
+# bash $ pip install faker
+# (1)faker 라는 패키지을 설치하기 위한 명령어 
+# (2) bash, cmd, shell 에서 실행한다
 
-# 2. Basic Usages
-from faker import Faker # 1 모듈을 사용하기 위한 코드이다
+
+
+#2. Basic Usages
+from faker import Faker # 1 faker패키지 안의 Faker 모듈 가져오기 위한 코드이다
 fake = Faker()          # 2 Faker는 클래스, fake는 인스턴스이다.
 fake.name()             # 3 name()은 fake의 속성이다.
 
-# 3. Localization
-
-fake = Faker()
-fake.name()
-# => 'shelly wilcox' (랜덤이므로 결과 값이 다를 수 있음)
-
-fake_ko = Faker('ko_KR')
-fake_ko.name()
-# => '박진우' (랜덤이므로 결과 값이 다를 수 있음)
-
+# 3.  Localization
 class Faker():
-    def __init__(self,fake):
+    def __init__(self,locale='en_US'):
         pass
 
-
-# 4. Seeding the Generator
-import random
-
-random.random() #=> 임의의 수
-random.random() #=> 임의의 수
-
-random.seed(7777) 
-random.random() # => 0.8170477907294282
-
-random.seed(7777) 
-random.random() # => 0.8170477907294282
-
+# 4.  Seeding the Generator
 fake = Faker('ko_KR')
-Faker.seed(4321)  # 클래스 메소드
-
-print(fake.name())  # 1
+Faker.seed(4321)
+# seed()는 클래스 메소드
+print(fake.name())  # 1  이도윤
 
 fake2 = Faker('ko_KR')
-print(fake2.name()) # 2 
+print(fake2.name()) # 2  이지후
 
-# 4. Seeding the Generator
 fake = Faker('ko_KR')
-fake.seed_instance(4321) # 인스턴스 메소드 
-
-print(fake.name())   # 1
+fake.seed_instance(4321)
+# seed_instance() 인스턴스 메소드 
+print(fake.name())   # 1 이도윤
 
 fake2 = Faker('ko_KR')
-print(fake2.name())  # 2
+print(fake2.name())  # 2 김광수
