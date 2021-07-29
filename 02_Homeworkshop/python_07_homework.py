2401099e7788bed7f1668119bbbfacca0a47ed9b
JUNYOUNG <roywnsdud31@gmail.com>
2021-07-29 17:32:51 +0900

    homework7 update

--- 02_Homeworkshop/python_07_homework.py
+++ 02_Homeworkshop/python_07_homework.py
@@ -1,19 +1,19 @@
 # 1. type Class
-dict() str() map() int() float()
+dict(), str(), map(),int(),float()
 
 # 2. Magic Method
 
-__init__    # 생성자를 생성하고
-__del__     # 생성자를 삭제하고
-__str__     # 문자열을 출력하고
-__repr__    # 
+# __init__    # 생성자를 생성하고
+# __del__     # 생성자를 삭제하고
+# __str__     # 문자열을 출력하고
+# __repr__    # 문자열을 출력하고
 
 
-# Instance Method
+# 3. Instance Method
 
 
 
-# Module Import
+# 4. Module Import
 
 # fibo.py
 def fibo_recursion(n):
@@ -23,6 +23,6 @@ def fibo_recursion(n):
         return fibo_recursion(n-1) + fibo_recursion(n-2)
 
 
-from fibo import fibo_recursion as recursion
+# from fibo import fibo_recursion as recursion
 
-recursion(4)
+# recursion(4)
