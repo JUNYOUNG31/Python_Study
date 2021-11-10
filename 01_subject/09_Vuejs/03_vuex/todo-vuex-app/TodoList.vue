2a6b7f4339c4d43014c4413e2dc1086bceee0a93
JUNYOUNG <roywnsdud31@gmail.com>
2021-11-10 21:25:18 +0900

    Vue vuex 추가

---
+++ 01_subject/09_Vuejs/03_vuex/todo-vuex-app/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="border border-danger">
+    <todo-list-item
+      v-for="todo in todos"
+      :key="todo.date"
+      :todo="todo"
+    >
+    </todo-list-item>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import TodoListItem from '@/components/TodoListItem'
+
+export default {
+  name: 'TodoList',
+  components: {
+    TodoListItem,
+  },
+  // 1번
+  // computed: { 
+  //   todos: function () { // computed 와 state 를 mapping
+  //     return this.$store.state.todos
+  //   }
+  // }
+  // 2번
+  // computed: mapState([
+  //   'todos',
+  // ])
+  // 최종
+  computed: {
+    ...mapState([
+      'todos',
+    ])
+  }
+}
+</script>
+
+<style>
+
+</style>
